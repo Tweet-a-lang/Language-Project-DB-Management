87140d72166eecc6ad8b9a746202ac4165abc4fd
Dennis <contact@dennisfoster.co.uk>
2017-10-25 15:59:07 +0100

    Adding seed-all feature with script: npm  run server:seed-all

--- back-end/seed/test.seed.js
+++ back-end/seed/test.seed.js
@@ -1,15 +1,49 @@
-const {Tweets} = require('../models/models.js');
+const {Tweets, Users} = require('../models/models.js');
+
 const testData = require('./exampleTweets.json');
+const testUsers = require('./exampleUsers.json');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const config = require('../server/config')
 
 
-function seedDataBaseTweets () {
-    Tweets.collection.drop()
+function seedDataBase () {
+    
     mongoose.connect(config.url, {useMongoClient: true})
 
-   const tweetPromises =  testData.map((tweet) => {
+    Tweets.collection.drop()
+    Users.collection.drop()
+
+    Promise.all(seedUsers())
+    .then(data => {
+        console.log('the collection of users was seeded with', data.length, 'users')
+        mongoose.disconnect()
+    })
+    .catch(console.error)
+
+    Promise.all(seedTweets())
+    .then(data => {
+        console.log('the collection of tweets was seeded with', data.length, 'tweets')
+        mongoose.disconnect()
+    })
+    .catch(console.error)
+}
+
+function seedUsers () {
+    
+    return testUsers.map((user) => {
+        return new Users({
+            name: user.name,
+            score: user.score,
+            avatar: user.avatar,
+            completedTweets: user.completedTweets
+        })
+        .save()
+    });
+}
+
+function seedTweets () {
+    return testData.map((tweet) => {
         return new Tweets({
           created_at: tweet.created_at,
           id: tweet.id,
@@ -19,17 +53,7 @@ function seedDataBaseTweets () {
           user_profile_image: tweet.user.profile_image_url 
         })
         .save()
-    })
-
-    return Promise.all(tweetPromises)
-    .then(data => {
-        console.log('the collection of tweets was seeded with', data.length, 'tweets')
-        mongoose.disconnect()
-    })
-    .catch(console.error)
+    });
 }
 
-seedDataBaseTweets();
-
-
-// module.exports = saveTestData;
+seedDataBase();
