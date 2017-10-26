e925c8e33854206484e9991e422f02f5b4fdb791
olie.chan.141@gmail.com <olie.chan.141@gmail.com>
2017-10-26 12:01:59 +0100

    add test for dev endpoint reset user

--- back-end/spec/server.spec.js
+++ back-end/spec/server.spec.js
@@ -22,5 +22,65 @@ describe('API', () => {
             .catch(console.error)
         }).timeout(5000)
     })
-    describe('GET')
+    describe('GET user/:username', () => {
+        it('returns with a status code of 200', () => {
+            return request(app)
+            .get('/api/user/olie')
+            .expect(200)
+            .then(res => {
+                const {name, score, completedTweets} = res.body;
+                expect(name).to.be.a('string');
+                expect(name).to.eql('olie');
+                expect(score).to.be.a('string');
+                expect(completedTweets).to.be.a('array');
+            })
+        })
+    });
+    describe.only('PATCH user/:username', () => {
+        it('returns with a status code of 200', () => {
+            const patchBody = {
+                completedTweets: ['test'],
+                score: 1
+            }
+            return request(app)
+            .patch('/api/user/olie')
+            .send(patchBody)
+            .expect(200)
+        })
+        it('returns an object containing the updated user', () => {
+            const patchBody = {
+                completedTweets: ['test'],
+                score: 1
+            }
+            return request(app)
+            .get('/dev/reset/olie')
+            .then(() => {
+                return request(app)
+                .patch('/api/user/olie')
+                .send(patchBody)
+                .expect(200)
+            })
+            .then(res => {
+                const {score, completedTweets} = res.body;
+                expect(score).to.equal(1)
+                expect(completedTweets).to.eql(['test'])
+            })
+        })
+    })
+});
+
+describe('DEV', () => {
+    describe('GET reset/:username', () => {
+        it('returns witha a status code of 200', () => {
+            return request(app)
+            .get('/dev/reset/olie')
+            .expect(200)
+            .then(res => {
+                const {name, score, completedTweets} = res.body;
+                expect(name).to.equal('olie');
+                expect(score).to.equal(0);
+                expect(completedTweets).to.eql([]);
+            })
+        })
+    })
 })
